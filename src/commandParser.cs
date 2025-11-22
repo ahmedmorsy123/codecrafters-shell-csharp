a9c3b7467aab4d0c302687754991099f1bbabd35
ahmed morsy <morsy0551@gmail.com>
2025-11-22 23:19:07 +0200

    add backslash escaping

--- src/commandParser.cs
+++ src/commandParser.cs
@@ -15,12 +15,59 @@ public class CommandParser
         StringBuilder currentPart = new StringBuilder();
         bool inSingleQuote = false;
         bool inDoubleQuote = false;
+        bool escaped = false;
 
         for (int i = 0; i < commandLine.Length; i++)
         {
             char c = commandLine[i];
 
-            if (c == '\'' && !inDoubleQuote)
+            if (escaped)
+            {
+                // Previous character was a backslash
+                if (inDoubleQuote)
+                {
+                    // In double quotes: only ", \, $, `, and newline can be escaped
+                    // For this stage, we only handle " and \
+                    if (c == '"' || c == '\\')
+                    {
+                        currentPart.Append(c);
+                    }
+                    else
+                    {
+                        // Backslash is literal for other characters
+                        currentPart.Append('\\');
+                        currentPart.Append(c);
+                    }
+                }
+                else if (inSingleQuote)
+                {
+                    // In single quotes: backslash has no special meaning
+                    // This shouldn't happen because we don't set escaped=true in single quotes
+                    currentPart.Append('\\');
+                    currentPart.Append(c);
+                }
+                else
+                {
+                    // Outside quotes: backslash escapes the next character
+                    currentPart.Append(c);
+                }
+                escaped = false;
+            }
+            else if (c == '\\')
+            {
+                // Backslash encountered
+                if (inSingleQuote)
+                {
+                    // In single quotes: backslash is literal
+                    currentPart.Append(c);
+                }
+                else
+                {
+                    // Outside quotes or in double quotes: mark as escaped
+                    escaped = true;
+                }
+            }
+            else if (c == '\'' && !inDoubleQuote)
             {
                 // Toggle single quote state (only if not inside double quotes)
                 inSingleQuote = !inSingleQuote;
@@ -47,13 +94,24 @@ public class CommandParser
             }
         }
 
+        // Handle trailing backslash (if any)
+        if (escaped)
+        {
+            currentPart.Append('\\');
+        }
+
         // Add the last part if there is one
         if (currentPart.Length > 0)
         {
             parts.Add(currentPart.ToString());
         }
 
-        // First part is the command name, rest are arguments
+        if (parts.Count == 0)
+        {
+            throw new ArgumentException("No command found");
+        }
+
+        // First part is the command name (can be quoted), rest are arguments
         string commandName = parts[0];
         string[] commandArgs = parts.Skip(1).ToArray();
 
